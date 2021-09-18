61b3e7e09b9a5f4a5ee24c715b42517fa176e1fd
Dipesh Patil <heydipesh28@gmail.com>
2021-09-18 20:35:10 +0530

    Added fix for false dark value

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,15 +5,15 @@ import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
+// import StartupProject from "./StartupProjects/StartupProject";
+// import Achievement from "./achievement/Achievement";
+// import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
+// import Talks from "./talks/Talks";
+// import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
 import Top from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
+// import Twitter from "./twitter-embed/twitter";
 import {StyleProvider} from "../contexts/StyleContext";
 import "./Main.scss";
 import Profile from "./profile/Profile";
@@ -22,28 +22,20 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isDark: false
+      isDark: true
     };
   }
 
   componentDidMount() {
-    if (localStorage.getItem("isDark") === null) {
-      const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-      localStorage.setItem("isDark", darkPref.matches);
-    }
-    this.setState({isDark: JSON.parse(localStorage.getItem("isDark"))});
+    localStorage.setItem("isDark", true);
+    this.setState({isDark: true});
   }
-  changeTheme = () => {
-    this.setState({isDark: !this.state.isDark}, () => {
-      localStorage.setItem("isDark", this.state.isDark);
-    });
-  };
 
   render() {
     return (
       <div className="dark-mode">
         <StyleProvider
-          value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
+          value={{isDark: this.state.isDark, changeTheme: null}}
         >
           <Header />
           <Greeting />
